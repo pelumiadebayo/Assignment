3b838adb601e140ab85dbc5ca182617756c5d043
sapphire1996 <adepelumi1996@gmail.com>
2020-02-08 10:54:59 +0100

    Add project files.

---
+++ StringReverser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Reverser
+{
+    public  class StringReverser
+    {
+        public static string ReverseString(string myString)
+        {
+            string reversedString = "";
+            int increment = 1;
+            //looping through the string backward and concatinating it into a new string
+            while (increment < myString.Length + 1)
+            {
+                reversedString += myString[myString.Length - increment];
+                increment++;
+            }
+            Console.WriteLine(reversedString);
+            Console.ReadLine();
+            return reversedString;
+        }
+
+    }
+}
