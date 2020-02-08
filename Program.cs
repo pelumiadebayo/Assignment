02792610878d3f8206da013571f37be2a7583f26
sapphire1996 <adepelumi1996@gmail.com>
2020-02-08 12:26:55 +0100

    calling the fuctions from the Main method

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace Reverser
     {
         static void Main(string[] args)
         {
+            //calling the ReverseString from the StringReverser class
             var reversedString = StringReverser.ReverseString("Pelumi");
             Console.WriteLine(reversedString);
             Console.ReadLine();
 
+            //calling the ReverseArryOfIntegers from the ArrayReverser class
             var reversedArray = ArrayReverser.ReverseArryOfIntegers(new int[] {1,2,3,4,5,5});
             foreach (var num in reversedArray)
             {
